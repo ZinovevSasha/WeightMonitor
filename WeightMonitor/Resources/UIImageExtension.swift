a6f6ffde08e3688040aa9a746f01ee87a8706ecc
ZinovevSasha <109625848+ZinovevSasha@users.noreply.github.com>
2023-05-07 23:50:19 +0200

    weight monitor

---
+++ WeightMonitor/Resources/UIImageExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIImageExtension.swift
+//  WeightMonitor
+//
+//  Created by Александр Зиновьев on 04.05.2023.
+//
+
+import UIKit
+
+extension UIImage {
+    static let arrowRight = UIImage(named: "arrowRight")
+    static let weightImage = UIImage(named: "weightImage")
+}
